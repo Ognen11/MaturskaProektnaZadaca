0f1000078e47c061e00f71235284301c1589380d
Ognen11 <ognjenduric11@gmail.com>
2025-02-24 15:03:48 +0100

    Add files via upload

--- Maturska_proektna_zadaca/index.js
+++ Maturska_proektna_zadaca/index.js
@@ -1,1 +1,10 @@
+window.addEventListener('scroll', function() {
+    var header = document.querySelector('.NavigationBar');
     
+    // Check if the scroll position is greater than 50px (adjust this value as needed)
+    if (window.scrollY > 50) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });
